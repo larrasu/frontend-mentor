ef2f986e383e480a8b2d0b14ffeac5cc769458c5
Mei <74983120+meixsu@users.noreply.github.com>
2023-01-31 17:47:22 +0800

    install new project

--- audiophile-ecommerce-website/nuxt.config.ts
+++ audiophile-ecommerce-website/nuxt.config.ts
@@ -1,4 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "Frontend Mentor | Audiophile E-commerce Website",
+      link: [{ rel: "icon", type: "image/png", href: "/favicon-32x32.png" }],
+    },
+  },
+  imports: {
+    dirs: ["stores"],
+  },
+  modules: [
+    "nuxt-headlessui",
+    "nuxt-icon",
+    "@nuxtjs/google-fonts",
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+  ],
+  headlessui: {
+    prefix: "",
+  },
+  googleFonts: {
+    families: {
+      Manrope: [400, 500, 700],
+    },
+  },
+  pinia: {
+    autoImports: ["defineStore", "storeToRefs"],
+  },
+});
