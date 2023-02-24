5a71743925f4451f4724500c2578d4bb7067ee15
Mei <74983120+meixsu@users.noreply.github.com>
2023-02-24 19:45:40 +0800

    index init

--- product-feedback-app/pages/index.vue
+++ product-feedback-app/pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div></div>
+  <div class="flex">
+    <Sidebar />
+    <section>
+      <Header />
+      <main>
+        <RequestCard v-for="request of requests" :request="request" />
+      </main>
+    </section>
+  </div>
 </template>
+
+<script setup>
+const requestStore = useRequestStore();
+const { requests } = storeToRefs(requestStore);
+
+await useAsyncData(async () => {
+  await requestStore.fetchRequests();
+});
+</script>
