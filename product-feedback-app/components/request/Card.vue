c7ec554e1dff81ad147d9d957faa62f451e0bdda
Mei <74983120+meixsu@users.noreply.github.com>
2023-02-24 19:45:30 +0800

    create request card

---
+++ product-feedback-app/components/request/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>
+      <p>{{ request.title }}</p>
+      <p>{{ request.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Request } from "~/types";
+const props = defineProps<{
+  request: Request;
+}>();
+const { request } = toRefs(props);
+</script>
