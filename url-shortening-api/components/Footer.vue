ae42703a8f8022bddd8df4494e41d28a2efa732f
Mei <74983120+meixsu@users.noreply.github.com>
2023-02-27 23:46:16 +0800

    🚧 work in progress

---
+++ url-shortening-api/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer aria-label="Site Footer" class="bg-tertiary">
+    <div>
+      <div class="grid">
+        <div></div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ url-shortening-api/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header aria-label="Site Header">
+    <NuxtLink to="/">
+      <div class="sr-only">Home</div>
+      <img src="~/assets/images/logo.svg" alt="Shortly Logo" />
+    </NuxtLink>
+    <div>
+      <nav aria-label="Site Navigation">
+        <ul>
+          <li v-for="item of menu">
+            <NuxtLink to="/">
+              {{ item }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+const menu = ["Features", "Pricing", "Resources"];
+</script>
