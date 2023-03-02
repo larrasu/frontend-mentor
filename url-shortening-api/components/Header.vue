f79b2eb89ceb1c982204d8f1ac736849d50e698e
Mei <74983120+meixsu@users.noreply.github.com>
2023-03-02 12:52:51 +0800

    🎉 complete header

--- url-shortening-api/components/Header.vue
+++ url-shortening-api/components/Header.vue
@@ -1,11 +1,14 @@
 <template>
-  <header aria-label="Site Header">
-    <NuxtLink to="/">
-      <div class="sr-only">Home</div>
-      <img src="~/assets/images/logo.svg" alt="Shortly Logo" />
-    </NuxtLink>
-    <div>
-      <nav aria-label="Site Navigation">
+  <header class="relative w-full px-6 pt-10">
+    <div
+      aria-label="Site Header"
+      class="flex items-center justify-between gap-2"
+    >
+      <NuxtLink to="/">
+        <div class="sr-only">Home</div>
+        <img src="~/assets/images/logo.svg" alt="Shortly Logo" />
+      </NuxtLink>
+      <nav aria-label="Site Navigation" class="md:ml-auto hidden">
         <ul>
           <li v-for="item of menu">
             <NuxtLink to="/">
@@ -14,10 +17,37 @@
           </li>
         </ul>
       </nav>
+      <div>
+        <button @click="showMenu = !showMenu">
+          <Icon name="heroicons:bars-3-20-solid" class="text-neutral w-6 h-6" />
+        </button>
+      </div>
     </div>
+
+    <div
+      @click="showMenu = false"
+      v-if="showMenu"
+      class="bg-black/25 absolute left-0 right-0 z-0 w-full h-full mt-0 cursor-pointer"
+    ></div>
+    <Transition
+      enter-active-class="transition duration-200 ease-out"
+      enter-from-class="translate-y-1 opacity-0"
+      enter-to-class="translate-y-0 opacity-100"
+      leave-active-class="transition duration-150 ease-in"
+      leave-from-class="translate-y-0 opacity-100"
+      leave-to-class="translate-y-1 opacity-0"
+    >
+      <Menu
+        v-if="showMenu"
+        :menu="menu"
+        @close="showMenu = false"
+        class="mt-[23px]"
+      />
+    </Transition>
   </header>
 </template>
 
 <script setup>
 const menu = ["Features", "Pricing", "Resources"];
+const showMenu = ref(false);
 </script>
