b6310fcc35340d165c13e7330d9423b84d2449f1
Larra <meixmei0000@gmail.com>
2023-03-05 14:02:32 +0800

    🏷️ add invoice type

---
+++ invoice-app/types/index.ts
@@ -0,0 +1,31 @@
+export interface Invoice {
+  id: String;
+  createdAt: Date;
+  paymentDue: Date;
+  description: String;
+  paymentTerms: Number;
+  clientName: String;
+  clientEmail: String;
+  status: String;
+  senderAddress: {
+    street: String;
+    city: String;
+    postCode: String;
+    country: String;
+  };
+  clientAddress: {
+    street: String;
+    city: String;
+    postCode: String;
+    country: String;
+  };
+  items: [
+    {
+      name: String;
+      quantity: Number;
+      price: Number;
+      total: Number;
+    }
+  ];
+  total: Number;
+}
