f8791d0f977c476edbaa3a162d7abf5e1e0829c5
Larra <meixmei0000@gmail.com>
2023-10-31 03:58:18 +0800

    ✨ feat: create hero component

---
+++ solutions/url-shortening/components/Hero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="flex flex-col items-center text-center px-6 py-4">
+    <div class="relative overflow-x-hidden w-full h-[337px]">
+      <img src="~/assets/images/illustration-working.svg" alt="Illustration working" class="absolute max-w-full">
+    </div>
+    <h1 class="text-dark font-bold text-[42px]/[48px] tracking-[-1.05px]">More than just shorter links
+    </h1>
+    <p class="mt-[15px] text-gray-dark text-lg/[30px] tracking-[0.123px]">Build your brand’s recognition and get detailed
+      insights
+      on how your links
+      are performing.
+    </p>
+    <Button label="Get Started" class="mt-8 text-xl" />
+  </div>
+</template>
+
