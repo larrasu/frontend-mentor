137658ee94970f22db0e69a5f178bdd90c50eb97
Larra <meixmei0000@gmail.com>
2023-10-31 02:58:15 +0800

    ✨ feat: add colors

--- solutions/url-shortening/tailwind.config.ts
+++ solutions/url-shortening/tailwind.config.ts
@@ -1,10 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2BD0D0",
+        gray: {
+          DEFAULT: "#EFF1F7",
+          dark: "#9E9AA8",
+        },
+        violet: {
+          DEFAULT: "#4B3F6B",
+          dark: "#3A3054",
+        },
+        dark: "#34313D",
+      },
+    },
   },
-  plugins: [],
-} satisfies Config
-
+  plugins: [require("@tailwindcss/forms")],
+} satisfies Config;
