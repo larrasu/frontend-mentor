43f2cf85e02a36737729ec13bbb241f38c8c4285
Larra <meixmei0000@gmail.com>
2023-10-31 03:58:08 +0800

    ✨ feat: create header component

---
+++ solutions/url-shortening/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <header>
+    <div class="flex items-center justify-between px-6 py-10">
+      <img src="~/assets/images/logo.svg" alt="Shortly" class="h-8" />
+
+      <button class="flex items-center justify-center">
+        <Icon name="heroicons:bars-3-20-solid" class="w-6 h-5" />
+      </button>
+    </div>
+  </header>
+</template>
+
