e8a07116dfb32a14a98965b958c216d2dd90c27e
Larra <meixmei0000@gmail.com>
2023-10-31 03:57:52 +0800

    ✨ feat: create Button component

---
+++ solutions/url-shortening/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue';
+
+const props = defineProps({
+  type: { type: String as PropType<ButtonHTMLAttributes['type']>, default: 'button' },
+  label: { type: String, default: null },
+  rounded: { type: String, default: 'full' }
+})
+
+type ButtonRound = 'full' | 'base' | string;
+
+const roundness: Record<ButtonRound, string> = {
+  full: 'rounded-full',
+  base: 'rounded-[5px]'
+}
+</script>
+<template>
+  <button :type="type" class="bg-primary text-white hover:bg-primary-hover font-bold transition px-10 py-4"
+    :class="$attrs, [roundness[rounded]]">
+    <slot>{{ label }}</slot>
+  </button>
+</template>
+
