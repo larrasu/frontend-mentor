67dec56192536dd5bae2ca36b6a4716e8fa612c0
Larra <meixmei0000@gmail.com>
2023-10-31 03:58:40 +0800

    ✨ feat: create Features component

---
+++ solutions/url-shortening/components/Features.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <!-- TODO: wip -->
+  <div class="bg-gray text-center px-6">
+    <div>
+      <h2 class="text-dark text-[28px]/[48px] tracking-[-0.7px] font-bold">Advanced Statistics</h2>
+      <p class="mt-4 text-gray-dark text-base/7 tracking-[-0.109px]">Track how your links are performing across the web
+        with
+        our advanced
+        statistics dashboard.</p>
+    </div>
+    <div class="mt-12">
+      <div class="relative bg-white rounded-[5px] flex flex-col items-center text-center px-8 py-10">
+        <div
+          class="absolute -top-10 left-1/2 -translate-x-1/2 bg-violet-dark w-[88px] h-[88px] rounded-full flex items-center justify-center">
+          <img src="~/assets/images/icon-brand-recognition.svg" alt="Brand Recognition" />
+        </div>
+        <h3 class="mt-[33px] text-dark text-[22px] font-bold">Brand Recognition</h3>
+        <p class="mt-3 text-gray-dark">Boost your brand recognition with each click. Generic links don’t mean a thing.
+          Branded
+          links
+          help instil confidence in your content.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
