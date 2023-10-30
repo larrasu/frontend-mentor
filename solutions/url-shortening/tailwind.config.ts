90179ba3c008610de14919496851dcdac2c1f4d2
Larra <meixmei0000@gmail.com>
2023-10-31 03:57:40 +0800

    🔧 chore: add colors and font

--- solutions/url-shortening/tailwind.config.ts
+++ solutions/url-shortening/tailwind.config.ts
@@ -5,16 +5,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#2BD0D0",
+        primary: { DEFAULT: "#2BD0D0", hover: "#9AE3E3" },
         gray: {
           DEFAULT: "#EFF1F7",
           dark: "#9E9AA8",
+        },
+        error: {
+          DEFAULT: "#F46363",
         },
         violet: {
           DEFAULT: "#4B3F6B",
           dark: "#3A3054",
         },
         dark: "#34313D",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
