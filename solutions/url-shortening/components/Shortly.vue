b0312a84b948f4c02f74d63915a92d4351077b71
Larra <meixmei0000@gmail.com>
2023-10-31 03:58:29 +0800

    ✨ feat: create Shortly component

---
+++ solutions/url-shortening/components/Shortly.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="bg-gradient-to-b from-white from-50% to-gray to-50% px-6 py-20">
+    <div class="bg-violet-dark rounded-[10px]">
+      <div class="grid gap-4 p-6">
+        <input type="text" name="url" id="url" placeholder="Shorten a link here..."
+          class="rounded-[5px] text-base/9 tracking-[0.12px]" />
+        <Button label="Shorten It!" rounded="base" />
+      </div>
+    </div>
+  </div>
+</template>
+
