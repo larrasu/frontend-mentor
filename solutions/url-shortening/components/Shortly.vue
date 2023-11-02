11c95b0d4f49454626afb214f23d78dac2bcca39
Larra <meixmei0000@gmail.com>
2023-11-02 15:00:24 +0800

    ✨ feat: add bg image

--- solutions/url-shortening/components/Shortly.vue
+++ solutions/url-shortening/components/Shortly.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
   <div class="bg-gradient-to-b from-white from-50% to-gray to-50% px-6 py-20">
-    <div class="bg-violet-dark rounded-[10px]">
+    <div class="bg-violet-dark bg-shorten-mobile bg-no-repeat bg-right-top rounded-[10px]">
       <div class="grid gap-4 p-6">
         <input type="text" name="url" id="url" placeholder="Shorten a link here..."
           class="rounded-[5px] text-base/9 tracking-[0.12px]" />
