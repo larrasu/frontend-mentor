9efd47954900ae3153546eb91c386564c57aec73
Larra <meixmei0000@gmail.com>
2023-11-02 14:59:57 +0800

    ✨ feat: create CTA

---
+++ solutions/url-shortening/components/CTA.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="bg-boost-mobile bg-violet-dark bg-no-repeat bg-cover">
+    <div class="flex flex-col items-center justify-center py-20">
+      <h2
+        class="text-white font-bold text-[28px]/[48px] tracking-[-0.7px] mb-4"
+      >
+        Boost your links today
+      </h2>
+      <Button label="Get Started" />
+    </div>
+  </div>
+</template>
